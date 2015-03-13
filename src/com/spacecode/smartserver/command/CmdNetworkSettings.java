e30e034b76519271bac4feb5f40f08e42d3bbfcf
Vincent <vincent.guilloux@spacecode.com>
2015-03-13 15:24:02 +0100

    * NetworkSettings improved. Now handling Windows. Linux part: refactored.

--- src/com/spacecode/smartserver/command/CmdNetworkSettings.java
+++ src/com/spacecode/smartserver/command/CmdNetworkSettings.java
@@ -4,11 +4,16 @@ import com.spacecode.smartserver.SmartServer;
 import com.spacecode.smartserver.helper.SmartLogger;
 import io.netty.channel.ChannelHandlerContext;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 
 /**
@@ -39,14 +44,18 @@ public class CmdNetworkSettings extends ClientCommand
 
         osName = osName.toLowerCase();
         
+        // if the OS is Linux, read the "interfaces" file to get the network settings
         if(osName.contains("linux"))
         {
             try
             {
                 List<String> networkInterfaces = 
                         Files.readAllLines(Paths.get("/etc/network/interfaces"), Charset.defaultCharset());
-                
-                String ipDevice = "", netmask = "", gateway = "";
+
+                Map<String, String> netConf = new HashMap<>();
+                netConf.put("address", "");
+                netConf.put("netmask", "");
+                netConf.put("gateway", "");
                 
                 for(String line : networkInterfaces)
                 {
@@ -58,40 +67,92 @@ public class CmdNetworkSettings extends ClientCommand
                         return;
                     }
                     
-                    if(line.contains("address"))
+                    for(String confKey : netConf.keySet())
                     {
-                        ipDevice = line.split(" ")[1];
-                    }
-                    
-                    else if(line.contains("netmask"))
-                    {
-                        netmask = line.split(" ")[1];
-                    }
-                    
-                    else if(line.contains("gateway"))
-                    {
-                        gateway = line.split(" ")[1];
+                        if(line.contains(confKey))
+                        {
+                            String[] fragments = line.split(" ");
+                            
+                            if(fragments.length >= 2)
+                            {
+                                netConf.put(confKey, fragments[1]);
+                            }
+                            
+                            break;
+                        }
                     }
                 }
 
-                SmartServer.sendMessage(ctx, ClientCommandRegister.NETWORK_SETTINGS, ipDevice, netmask, gateway);
+                SmartServer.sendMessage(ctx, ClientCommandRegister.NETWORK_SETTINGS, 
+                        netConf.get("address"), netConf.get("netmask"), netConf.get("gateway"));
                 return;
             } catch (IOException ioe)
             {
                 SmartLogger.getLogger().log(Level.SEVERE, "An I/O error occurred when getting Network Settings", ioe);
             }
-
-            SmartServer.sendMessage(ctx, ClientCommandRegister.NETWORK_SETTINGS, "null");
         }
         
         else if(osName.contains("windows"))
         {
-            
+            ProcessBuilder processBuilder = new ProcessBuilder("cmd.exe", "/c", "ipconfig");
+            processBuilder.redirectErrorStream(true);
+
+            try
+            {
+                Process process = processBuilder.start();
+                InputStream is = process.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+
+                Map<String, String> netConf = new HashMap<>();
+                netConf.put("ip address", "");
+                netConf.put("subnet", "");
+                netConf.put("gateway", "");
+
+                String line = bufferedReader.readLine();
+                boolean isEth0 = false;
+
+                while (line != null)
+                {
+                    line = line.toLowerCase();
+
+                    if(line.contains("adapter"))
+                    {
+                        isEth0 = line.contains("local area connection");
+                    }
+
+                    if(isEth0)
+                    {
+                        for (String keyConf : netConf.keySet())
+                        {
+                            if (line.contains(keyConf))
+                            {
+                                String[] fragments = line.split(": ");
+
+                                if (fragments.length >= 2)
+                                {
+                                    netConf.put(keyConf, fragments[1].trim());
+                                }
+                            }
+                        }
+                    }
+
+                    line = bufferedReader.readLine();
+                }
+
+                SmartServer.sendMessage(ctx, ClientCommandRegister.NETWORK_SETTINGS,
+                        netConf.get("ip address"), netConf.get("subnet"), netConf.get("gateway"));
+                return;
+            } catch (IOException ioe)
+            {
+                SmartLogger.getLogger().log(Level.SEVERE, "An I/O error occurred when getting Network Settings", ioe);
+            }
         }
         
         else
         {
             SmartLogger.getLogger().severe("Unknown OS: Unable to get the network settings.");
         }
+
+        SmartServer.sendMessage(ctx, ClientCommandRegister.NETWORK_SETTINGS, "null");
     }
 }
